ca7f1c0d9ff2d70afdd214c168b1813dd40dd7ef
qualorm <qualorm@protonmail.com>
2020-12-11 15:57:51 +0100

    New version of days 1-11

--- old/1_1.py
+++
@@ -1,7 +0,0 @@
-with open("day1.txt","r") as file:
-    nums = [int(line.strip()) for line in file]
-    # print(nums[:10])
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i]+nums[j] == 2020:
-                print(f"{nums[i]} * {nums[j]} = {nums[i]*nums[j]}")
