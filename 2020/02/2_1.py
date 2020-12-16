0523e36fc0cb011d33dd8d607014456bdc878714
qualorm <37869895+qualorm@users.noreply.github.com>
2020-12-16 13:29:01 +0100

    Password Philosophy

--- 2020/02/2_1.py
+++ 2020/02/2_1.py
@@ -7,7 +7,7 @@ with open("day2.txt","r") as file:
         conds, passwd = line.strip().split(":")
         n, l = conds.split(" ")
         n_low, n_high = int(n.split("-")[0]), int(n.split("-")[1])
-        # print(n_low, n_high)
+        
         c = 0
         for letter in passwd:
             if letter == l:
