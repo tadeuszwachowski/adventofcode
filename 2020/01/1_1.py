1f6b952f6b46a289eb5a9465abc4e12bf413d3b1
qualorm <37869895+qualorm@users.noreply.github.com>
2020-12-16 13:27:19 +0100

    Report Repair

--- 2020/01/1_1.py
+++ 2020/01/1_1.py
@@ -1,6 +1,7 @@
 with open("day1.txt","r") as file:
+    
     nums = [int(line.strip()) for line in file]
-    # print(nums[:10])
+
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j] == 2020:
