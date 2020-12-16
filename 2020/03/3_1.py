ea17013970c3b97670d75d1e57b627173e6e49db
qualorm <37869895+qualorm@users.noreply.github.com>
2020-12-16 13:30:33 +0100

    Toboggan Trajectory

--- 2020/03/3_1.py
+++ 2020/03/3_1.py
@@ -1,7 +1,9 @@
 with open("day3.txt","r") as file:
+    
     n_trees = 0
     x = 3
     next(file) # next line
+    
     for row in file:
         row = row.strip("\n")
         print(row)
@@ -9,4 +11,5 @@ with open("day3.txt","r") as file:
             n_trees += 1
         x += 3
         x %= len(row)
+
     print(n_trees)  
