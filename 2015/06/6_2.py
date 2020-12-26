345074d572599357567c4600b6604754f73bf4a7
qualorm <37869895+qualorm@users.noreply.github.com>
2020-12-26 12:21:19 +0100

    Probably a Fire Hazard

---
+++ 2015/06/6_2.py
@@ -0,0 +1,32 @@
+lights = [ [0 for i in range(1000)]
+                for j in range(1000) ]
+
+with open('input.txt') as file:
+    f = file.read().splitlines()
+
+    for line in f:
+        ins = line.split(' ')[-4] 
+        ll = list(map(int,line.split(' ')[-3].split(',')))
+        hr = list(map(int,line.split(' ')[-1].split(',')))
+
+        if ins == 'on':
+            for i in range(ll[0],hr[0]+1):
+                for j in range(ll[1],hr[1]+1):
+                    lights[i][j] += 1
+        
+        if ins == 'off':
+            for i in range(ll[0],hr[0]+1):
+                for j in range(ll[1],hr[1]+1):
+                    lights[i][j] -= 1
+                    if lights[i][j] < 0:
+                        lights[i][j] = 0
+
+        if ins == 'toggle':
+            for i in range(ll[0],hr[0]+1):
+                for j in range(ll[1],hr[1]+1):
+                    lights[i][j] += 2 
+
+    ans = 0
+    for row in lights:
+        ans += sum(row)
+    print('Part 2: ', ans)
