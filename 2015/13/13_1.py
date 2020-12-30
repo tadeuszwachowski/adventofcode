0c846ef31b8531af8de1538b6504604400018345
qualorm <37869895+qualorm@users.noreply.github.com>
2020-12-30 14:12:27 +0100

    Knights of the Dinner Table

---
+++ 2015/13/13_1.py
@@ -0,0 +1,48 @@
+import itertools
+
+with open('input.txt') as file:
+    f = file.read().splitlines()
+
+relations = {}
+guests = []
+
+for line in f:
+    name = line.split(' ')[0]
+    neighbor = line.split(' ')[10][:-1]
+    
+    if line.split(' ')[2] == 'gain':
+        happiness = int(line.split(' ')[3])
+    elif line.split(' ')[2] == 'lose':
+        happiness = -int(line.split(' ')[3])
+
+    if name not in guests:
+        guests.append(name)
+
+    if name not in relations.keys():
+        relations[name] = {neighbor: happiness}
+    else:
+        relations[name][neighbor] = happiness
+
+# print(relations)
+layouts = itertools.permutations(guests,len(guests))
+
+best_table = ()
+max_happiness = 0
+for table in layouts:
+    happiness = 0
+    # print(table)
+    for i in range(len(table)):
+        if i == 0:
+            n1, n2 = i+1, len(table)-1
+        elif i == len(table)-1:
+            n1, n2 = i-1, 0
+        else:
+            n1, n2 = i-1, i+1
+        value1 = relations[table[i]][table[n1]]
+        value2 = relations[table[i]][table[n2]]
+        happiness += value1+value2
+    if happiness > max_happiness:
+        max_happiness = happiness
+        best_table = table
+
+print(best_table, max_happiness)
