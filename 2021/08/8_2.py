d5e7e71370826d1c611ca673355b281caedc60e0
tadeuszwachowski <37869895+tadeuszwachowski@users.noreply.github.com>
2021-12-08 11:54:02 +0100

    Seven Segment Search

--- 2021/08/8_2.py
+++ 2021/08/8_2.py
@@ -4,14 +4,19 @@ with open('input.txt') as f:
         wires, output = line.strip().split(' | ')
         wires = wires.split()
         output = output.split()
-        # print(wires)
+
+        # store determined segments here
         displays = {0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: ''}
         # if len == 5 append to left triple, if len == 6 append to right triple
         left_triple = []
         right_triple = []
-        # first find the known valuesby len: 
+        
+        # first find the known value sby len: 
         # 1, 4, 7, 8 
         # group [2,3,5] (len5), [0,6,9] (len6)
+        '''
+        use the segments.png diagram to get better understanding
+        '''
         for w in wires:
             l = len(w)
             if l == 2: displays[1] = set(w)
@@ -20,6 +25,7 @@ with open('input.txt') as f:
             if l == 5: left_triple.append(set(w))
             if l == 6: right_triple.append(set(w))
             if l == 7: displays[8] = set(w)
+
         # first we sort the [0,6,9] group
         for w in right_triple:
             if displays[4].issubset(set(w)): 
@@ -29,6 +35,7 @@ with open('input.txt') as f:
                     displays[0] = set(w)
                 else:
                     displays[6] = set(w)
+
         # then we sort the [2,3,5] group
         for w in left_triple:
             if displays[7].issubset(set(w)): 
@@ -38,17 +45,13 @@ with open('input.txt') as f:
                     displays[5] = set(w)
                 else:
                     displays[2] = set(w)
-        # print(displays)
 
+        # decode the digits after "|", parse to int and add to sum 
         digits = ''
         for n in output:
             s = set(n)
             for i, key in enumerate(displays.values()):
                 if s == key:
                     digits = digits + str(i)
-        # print(digits)
         sum_of_numbers += int(digits)
 print(sum_of_numbers)
-        
-
-# print(count)
